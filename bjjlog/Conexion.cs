421a40897e99745b10f8874db2680b58cf420c59
Andres Borja <andresborjac2008@gmail.com>
2023-07-11 13:56:42 -0500

    Add: agregar logica a conexion

--- bjjlog/Conexion.cs
+++ bjjlog/Conexion.cs
@@ -16,5 +16,23 @@ namespace bjjlog
         {
             InitializeComponent();
         }
+
+        private void btnguardar_Click(object sender, EventArgs e)
+        {
+            if (txtdatabase.Text == "" || txtserver.Text == "")
+            {
+                MessageBox.Show("Por favor llene todos los campos");
+            }
+            else
+            {
+                string filePath = Environment.CurrentDirectory + @"\datosserver.txt";
+                File.WriteAllText(filePath, string.Empty);
+                using (StreamWriter sw = new StreamWriter(filePath))
+                {
+                    sw.WriteLine($"{txtserver.Text},{txtdatabase.Text}");
+                }
+                Application.Restart();
+            }
+        }
     }
 }
