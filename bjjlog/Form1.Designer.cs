906493d3b9fd9621ffa57d4ce5e5f4d2e1c5f26a
Andres Borja <andresborjac2008@gmail.com>
2023-07-10 22:28:23 -0500

    Creacion de inicio de sesion

--- bjjlog/Form1.Designer.cs
+++ bjjlog/Form1.Designer.cs
@@ -28,12 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            tableLayoutPanel1 = new TableLayoutPanel();
+            label1 = new Label();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            tableLayoutPanel3 = new TableLayoutPanel();
+            tableLayoutPanel4 = new TableLayoutPanel();
+            button1 = new Button();
+            txtpass = new TextBox();
+            txtusuario = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            button2 = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            tableLayoutPanel3.SuspendLayout();
+            tableLayoutPanel4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 1);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 15F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 85F));
+            tableLayoutPanel1.Size = new Size(432, 261);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = DockStyle.Fill;
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(426, 39);
+            label1.TabIndex = 0;
+            label1.Text = "INICIO DE SESION";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 3;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 60F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel2.Controls.Add(tableLayoutPanel3, 1, 1);
+            tableLayoutPanel2.Controls.Add(button2, 1, 2);
+            tableLayoutPanel2.Dock = DockStyle.Fill;
+            tableLayoutPanel2.Location = new Point(3, 42);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 3;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 15F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 70F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 15F));
+            tableLayoutPanel2.Size = new Size(426, 216);
+            tableLayoutPanel2.TabIndex = 1;
+            // 
+            // tableLayoutPanel3
+            // 
+            tableLayoutPanel3.ColumnCount = 3;
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel3.Controls.Add(tableLayoutPanel4, 1, 4);
+            tableLayoutPanel3.Controls.Add(txtpass, 1, 3);
+            tableLayoutPanel3.Controls.Add(txtusuario, 1, 1);
+            tableLayoutPanel3.Controls.Add(label2, 1, 0);
+            tableLayoutPanel3.Controls.Add(label3, 1, 2);
+            tableLayoutPanel3.Dock = DockStyle.Fill;
+            tableLayoutPanel3.Location = new Point(88, 35);
+            tableLayoutPanel3.Name = "tableLayoutPanel3";
+            tableLayoutPanel3.RowCount = 5;
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 17.9310341F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 19.3103447F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 20.6896553F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 18.62069F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 23.4482765F));
+            tableLayoutPanel3.Size = new Size(249, 145);
+            tableLayoutPanel3.TabIndex = 0;
+            // 
+            // tableLayoutPanel4
+            // 
+            tableLayoutPanel4.ColumnCount = 3;
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 80F));
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            tableLayoutPanel4.Controls.Add(button1, 1, 0);
+            tableLayoutPanel4.Dock = DockStyle.Fill;
+            tableLayoutPanel4.Location = new Point(65, 114);
+            tableLayoutPanel4.Name = "tableLayoutPanel4";
+            tableLayoutPanel4.RowCount = 1;
+            tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel4.Size = new Size(118, 28);
+            tableLayoutPanel4.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Fill;
+            button1.Location = new Point(14, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(88, 22);
+            button1.TabIndex = 0;
+            button1.Text = "INICIAR";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // txtpass
+            // 
+            txtpass.Dock = DockStyle.Fill;
+            txtpass.Location = new Point(65, 87);
+            txtpass.Name = "txtpass";
+            txtpass.Size = new Size(118, 23);
+            txtpass.TabIndex = 1;
+            // 
+            // txtusuario
+            // 
+            txtusuario.Dock = DockStyle.Fill;
+            txtusuario.Location = new Point(65, 29);
+            txtusuario.Name = "txtusuario";
+            txtusuario.Size = new Size(118, 23);
+            txtusuario.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Dock = DockStyle.Fill;
+            label2.Location = new Point(65, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(118, 26);
+            label2.TabIndex = 3;
+            label2.Text = "USUARIO";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Dock = DockStyle.Fill;
+            label3.Location = new Point(65, 54);
+            label3.Name = "label3";
+            label3.Size = new Size(118, 30);
+            label3.TabIndex = 4;
+            label3.Text = "CONTRASEÑA";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            button2.Dock = DockStyle.Fill;
+            button2.Location = new Point(88, 186);
+            button2.Name = "button2";
+            button2.Size = new Size(249, 27);
+            button2.TabIndex = 1;
+            button2.Text = "SALIR";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(432, 261);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
+            Text = "LogIn";
+            Load += Form1_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel3.ResumeLayout(false);
+            tableLayoutPanel3.PerformLayout();
+            tableLayoutPanel4.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private TableLayoutPanel tableLayoutPanel3;
+        private TableLayoutPanel tableLayoutPanel4;
+        private Button button1;
+        private TextBox txtpass;
+        private TextBox txtusuario;
+        private Label label2;
+        private Label label3;
+        private Button button2;
     }
 }
--- bjjlog/Form1.cs
+++ bjjlog/Form1.cs
@@ -6,5 +6,10 @@ namespace bjjlog
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
